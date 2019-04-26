40a93a7be30bc3b4f720cb911dba628f75ad2d1f
Neknu <Neknu@users.noreply.github.com>
2019-04-26 10:57:29 +0300

    delete smth
    delete smth

---
+++ module1/cmake-build-debug/CMakeFiles/module1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/module1.dir/main.cpp.obj"
+  "module1.pdb"
+  "module1.exe"
+  "module1.exe.manifest"
+  "libmodule1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/module1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
